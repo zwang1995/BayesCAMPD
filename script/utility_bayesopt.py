dc5a20b7c689e7d2ebc2fd58a82ad38880974bb3
Zihao Wang <wangzihao1995@live.com>
2025-11-24 16:51:46 +0800

    Add files via upload

---
+++ script/utility_bayesopt.py
@@ -0,0 +1,177 @@
+# Created on 25 Jan 2023 by Zihao Wang
+# Functions used for Bayesian optimization
+
+from utility_common import logit
+from process_simulation_CAPD import run_simulation_CAPD
+from process_simulation_CAMPD import run_simulation_CAMPD
+
+import random
+import joblib
+import numpy as np
+import scipy.stats as st
+from smt.sampling_methods import Random, LHS
+from smt.applications.mixed_integer import MixedIntegerContext, FLOAT, ORD
+
+
+def sampling(params, n_sample, solvent_list, sampling_method):
+    considerSolvent = params["considerSolvent"]
+    s_ub = None
+    if considerSolvent:
+        s_ub = len(solvent_list) - 1
+        xtypes = [ORD, ORD, FLOAT, FLOAT, FLOAT, ORD, FLOAT, FLOAT]
+        xlimits = [(0, s_ub)] + params["bound_pro"]
+    else:
+        xtypes = [ORD, FLOAT, FLOAT, FLOAT, ORD, FLOAT, FLOAT]
+        xlimits = params["bound_pro"]
+    xlimits = [xlimit if xtype == FLOAT else [xlimit[0], xlimit[1]] for (xlimit, xtype) in
+               zip(xlimits, xtypes)]
+
+    mixint = MixedIntegerContext(xtypes, xlimits)
+    samp = None
+    if sampling_method == "random":
+        samp = mixint.build_sampling_method(Random)
+    elif sampling_method == "latin_hypercube":
+        samp = mixint.build_sampling_method(LHS, random_state=params["random_seed"])
+
+    xt = samp(n_sample)
+    solvent_alias = None
+    if considerSolvent:
+        x_pro = xt[:, 1:]
+        x_sol = [random.choice(np.arange(0, s_ub + 1)) for _ in range(n_sample)]
+        solvent_alias = [solvent_list[i] for i in np.array(x_sol, dtype=int)]
+    else:
+        solvent_alias = [params["solvent"]] * n_sample
+        x_pro = xt
+    x_pro = np.round(x_pro, 4)
+    return solvent_alias, x_pro
+
+
+def valid_sampling(params, aspen_plus_, mol_list, input_, file_txt, label):
+    alias = None
+    considerSolvent = params["considerSolvent"]
+    useLogit = params["useLogit"]
+    n_reconnect = params["n_reconnect"]
+    puri_lb_, puri_ub_, puri_spec_ = params["puri_lb"], params["puri_ub"], params["puri_spec"]
+    if useLogit:
+        puri_lb_, puri_ub_, puri_spec_ = logit(puri_lb_), logit(puri_ub_), logit(puri_spec_)
+
+    mol_alias_in, x_pro_in, g1_in, g2_in, duty_in, tac_in = input_
+    mol, x_pro = sampling(params, 1, mol_list, sampling_method="random")
+    if considerSolvent:
+        (g1, y1, g2, y2, run_error, _, _, tac), time_cost = \
+            run_simulation_CAMPD(aspen_plus_, mol, x_pro, file_txt, label, n_reconnect)
+    else:
+        (g1, y1, g2, y2, run_error, _, _, tac), time_cost = \
+            run_simulation_CAPD(aspen_plus_, mol, x_pro, file_txt, label)
+    if g1 is not None:
+        if useLogit:
+            g1, g2 = logit(g1), logit(g2)
+        if (not run_error) & ((puri_lb_ <= g1) & (g1 <= puri_ub_)) & ((puri_lb_ <= g2) & (g2 <= puri_ub_)):
+            if considerSolvent:
+                alias = mol[0]
+            else:
+                alias = params["solvent"]
+            mol_alias_in.append(alias)
+            x_pro_in = np.vstack((x_pro_in, x_pro))
+            g1_in = np.append(g1_in, g1)
+            g2_in = np.append(g2_in, g2)
+            duty_in = np.append(duty_in, y1 + y2)
+            tac_in = np.append(tac_in, tac)
+    output = (mol_alias_in, x_pro_in, g1_in, g2_in, duty_in, tac_in)
+    return output, time_cost, alias
+
+
+class BOConvergence:
+    def __init__(self, es_patience=10, bo_patience=10):
+        self.es_patience, self.bo_patience = es_patience, bo_patience
+        self.best = None
+        self.counter = 0
+        self.stopBO = False
+        self.getExtraSample = False
+        self.optimum = None
+
+    def update_best(self, best_known):
+        if best_known is not None:
+            if self.best is None:
+                self.best = best_known
+            else:
+                if best_known < self.best:
+                    self.best = best_known
+                    self.counter = 0
+
+    def __call__(self, new):
+        if self.best is None:
+            self.best = new
+        if new < self.best:
+            self.best = new
+            self.counter = 0
+            self.getExtraSample = False
+        else:
+            self.counter += 1
+            if self.counter >= self.bo_patience:
+                self.stopBO = True
+                print(f"\t- terminated as no improvements were observed in {self.bo_patience} iterations")
+
+
+class BOMonitor:
+    def __init__(self):
+        self.record = []
+
+    def update(self, keys, values):
+        result = {key: value for (key, value) in zip(keys, values)}
+        self.record.append(result)
+
+    def save(self, path):
+        joblib.dump(self.record, path)
+
+
+def expected_improvement(mu, sigma, mu_opt, obj="MAX"):
+    EI = None
+    if obj == "MAX":
+        EI = (mu - mu_opt) * st.norm.cdf((mu - mu_opt) / sigma) + sigma * st.norm.pdf((mu - mu_opt) / sigma)
+    elif obj == "MIN":
+        EI = (mu_opt - mu) * st.norm.cdf((mu_opt - mu) / sigma) + sigma * st.norm.pdf((mu_opt - mu) / sigma)
+    return EI
+
+
+def probability_of_improvement(mu, sigma, mu_opt, obj="MAX"):
+    PI = None
+    if obj == "MAX":
+        PI = st.norm.cdf((mu - mu_opt) / sigma)
+    elif obj == "MIN":
+        PI = st.norm.cdf((mu_opt - mu) / sigma)
+    return PI
+
+
+def upper_confidence_bound(mu, sigma, lamb=1.0, obj="MAX"):
+    UCB = None
+    if obj == "MAX":
+        UCB = mu + lamb * sigma
+    elif obj == "MIN":
+        UCB = mu - lamb * sigma
+    return UCB
+
+
+def acquisition_func(x, params, model, y_tr_best):
+    ac_mode = params["ac_mode"]
+    x = x.reshape(1, -1)
+
+    obj = model.obj
+    f, f_std = model.predict(x, return_std=True)
+
+    af_value = None
+    if ac_mode == "PI":
+        PI = probability_of_improvement(f, f_std, y_tr_best, obj=obj)
+        af_value = -PI
+    elif ac_mode == "EI":
+        EI = expected_improvement(f, f_std, y_tr_best, obj=obj)
+        af_value = -EI
+    elif ac_mode == "UCB":
+        UCB = upper_confidence_bound(f, f_std, 1, obj=obj)
+        if model.obj == "MAX":
+            af_value = -UCB
+        elif model.obj == "MIN":
+            af_value = UCB
+    elif ac_mode == "MIN":
+        af_value = f
+    return af_value
---
+++ script/utility_params.py
@@ -0,0 +1,80 @@
+# Created on 04 Sep 2023 by Zihao Wang
+# Parameters and configurations for Computer-Aided Molecular and Process Design
+
+from utility_common import create_directory
+
+
+def get_params(target, strategy, considerSolvent, saveInitialData=True, runBayesCAMPD=True, solvent=None,
+               extra_sample_num=15, early_stop_patience=20):
+    params = {
+        "target": target,
+        "strategy": strategy,
+        "considerSolvent": considerSolvent,
+        "saveInitialData": saveInitialData,
+        "runBayesCAMPD": runBayesCAMPD,
+        "random_seed": 42,
+        "sampling_method": "latin_hypercube",  # ["random", "latin_hypercube"]
+        "optim_method": "DE",  # ["DE", "GA"]
+        "normInput": False,
+        "normOutput": True,
+        "useLogit": True,
+        "extra_sample_num": extra_sample_num,
+        "early_stop_patience": early_stop_patience,
+
+        "file_mol": "../data/Property_UNIFAC.csv",
+        "file_bkp": "../simulation/ExtractiveDistillation_T1T2.bkp",
+
+        "puri_lb": 0.,  # [0., 0.5]
+        "puri_ub": 1.,
+        "puri_spec": 0.995,
+
+        "col_mol": ["S", "CP", "HV"],
+        "col_pro": ["NStage_T1", "RR_T1", "TopPres_T1", "StoF", "NStage_T2", "RR_T2", "TopPres_T2"],
+
+        "solvent": solvent,
+    }
+
+    if considerSolvent:
+        params["list_n_sample"] = [128, 256, 384, 512, 640, 768, 896, 1024]
+        if strategy == "OneShot":
+            params["list_n_sample"] = [128, 256, 384, 512, 640, 768, 896, 1024, 1536, 2048, 2560, 3072, 3584, 4096]
+    else:
+        params["list_n_sample"] = [128, 256, 384, 512, 640, 768, 896, 1024]
+        if strategy == "OneShot":
+            params["list_n_sample"] = [128, 256, 384, 512, 640, 768, 896, 1024]
+    params["active_list_n_sample"] = params["list_n_sample"]
+
+    if strategy == "Bayesian":
+        params["n_reconnect"] = 32
+        params["n_iter"] = 100
+        params["n_trial"] = 5
+        params["ac_mode"] = "EI"
+    elif strategy == "OneShot":
+        params["n_reconnect"] = 32
+        params["n_iter"] = 1
+        params["n_trial"] = 20
+        params["ac_mode"] = "MIN"
+
+    params["bound_pro"] = [(40, 80), (1, 10), (3.5, 6), (1, 8), (8, 20), (0.2, 2), (2.5, 5)]
+    params["bound"] = None
+    mol_integrality = [0] * len(params["col_mol"])
+    pro_integrality = [1, 0, 0, 0, 1, 0, 0]
+    x_name_mol = ["S", "CP", "HV"]
+    x_name_pro = ["N1", "RR1", "P1", "StoF", "N2", "RR2", "P2"]
+
+    if considerSolvent:
+        params["integrality"] = mol_integrality + pro_integrality
+        params["x_name"] = x_name_mol + x_name_pro
+    else:
+        params["integrality"] = pro_integrality
+        params["x_name"] = x_name_pro
+
+    str_task = "CAMPD" if considerSolvent else "CAPD"
+    str_sol = f"_{solvent}" if solvent is not None else ""
+    params["path_out"] = create_directory(f"../model/{strategy}_{str_task}{str_sol}/")
+    params["path_data"] = create_directory(f"../model/Bayesian_{str_task}{str_sol}/data/")
+    params["path_script"] = params["path_out"] + f"script_{target}{str_sol}"
+    params["file_log"] = params["path_out"] + f"history_{target}{str_sol}.log"
+    params["file_params"] = params["path_out"] + f"params_{target}{str_sol}.json"
+
+    return params
